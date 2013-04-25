de1ec303fe9265d5141129ce03c62bcd03fe0c7d
Luis Ibanez <luis.ibanez@kitware.com>
2013-04-25 08:42:27 -0400

    Added threshold filter and writer.

--- image-processing/itkSurfaceExtraction.cxx
+++ image-processing/itkSurfaceExtraction.cxx
@@ -17,20 +17,30 @@
  *=========================================================================*/
 
 #include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
 #include "itkBinaryThresholdImageFilter.h"
 #include "itkAntiAliasBinaryImageFilter.h"
 
 int main( int argc, char * argv [] )
 {
+
+  if( argc < 3 )
+    {
+    std::cerr << "Usage" << std::endl;
+    std::cerr << argv[0] << "InputImageFile   labelValue" << std::endl;
+    return EXIT_FAILURE;
+    }
+
   const unsigned int Dimension = 3;
 
-  typedef unsigned char                                       InputPixelType;
+  typedef unsigned short                                      InputPixelType;
   typedef itk::Image< InputPixelType, Dimension >             InputImageType;
 
   typedef itk::ImageFileReader< InputImageType > ReaderType;
 
   ReaderType::Pointer reader = ReaderType::New();
   reader->SetFileName( argv[1] );
+
   try
     {
     reader->Update();
@@ -40,13 +50,29 @@ int main( int argc, char * argv [] )
     std::cerr << excp << std::endl;
     }
 
+  const InputPixelType labelValue = atoi( argv[2] );
 
-  typedef itk::Image< unsigned char, Dimension >              BinaryImageType;
-  typedef itk::Image< double, Dimension >                     RealImageType;
+  typedef unsigned char                                 BinaryPixelType;
+  typedef double                                        RealPixelType;
+
+  typedef itk::Image< BinaryPixelType, Dimension >      BinaryImageType;
+  typedef itk::Image< RealPixelType, Dimension >        RealImageType;
 
   typedef itk::BinaryThresholdImageFilter< InputImageType, BinaryImageType >  ThresholdFilterType;
 
   ThresholdFilterType::Pointer binaryThresholdFilter = ThresholdFilterType::New();
+
+  const BinaryPixelType outsideValue = 0;
+  const BinaryPixelType insideValue = 255;
+
+  binaryThresholdFilter->SetOutsideValue( outsideValue );
+  binaryThresholdFilter->SetInsideValue(  insideValue  );
+
+  const InputPixelType lowerThreshold = labelValue;
+  const InputPixelType upperThreshold = labelValue;
+
+  binaryThresholdFilter->SetLowerThreshold( lowerThreshold );
+  binaryThresholdFilter->SetUpperThreshold( upperThreshold );
 
   binaryThresholdFilter->SetInput( reader->GetOutput() );
 
@@ -55,9 +81,24 @@ int main( int argc, char * argv [] )
   AntiAliasFilterType::Pointer antiAliasFilter = AntiAliasFilterType::New();
 
   antiAliasFilter->SetNumberOfIterations(50);
+
   antiAliasFilter->SetInput( binaryThresholdFilter->GetOutput() );
 
-  antiAliasFilter->Update();
+
+  typedef itk::ImageFileWriter< RealImageType >  WriterType;
+
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetInput( antiAliasFilter->GetOutput() );
+  writer->SetFileName( argv[3] );
+
+  try
+    {
+    writer->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << excp << std::endl;
+    }
 
   return EXIT_SUCCESS;
 }
